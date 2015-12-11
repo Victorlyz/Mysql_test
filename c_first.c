d519b429e1aff07f4cb4701cd96f50f7e3d006f2
liuyz <zhuan123tou.gmail.com>
2015-12-11 18:14:01 +0800

    make some functions
    1.Mysql_connect
    2.Mysql_insert...

--- c_first.c
+++ c_first.c
@@ -7,15 +7,9 @@ int main(int argc, char *argv[]) {
     long int   snResult = 0;
     char       chOption[1+1];
     char       chBuff[100];
-    MYSQL      *pMysql;
-    MYSQL_RES  *pRes;
-    MYSQL_ROW  *pRow;
-    MYSQL      *psnResult ;
-
-    char chServer[] = "localhost";
-    char chUser[] = "root";
-    char chPassword[] = "test123";
-    char chDatabase[] = "db_test";
+    MYSQL      *pMysql = NULL;
+    MYSQL_RES  *pRes = NULL;
+    MYSQL_ROW  *pRow = NULL;
 
     memset(chOption, 0x00, sizeof(chOption));
     memset(chBuff, 0x00, sizeof(chBuff));
@@ -26,47 +20,86 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     pMysql = mysql_init(NULL);
-    if(NULL == pMysql) {
+    if(NULL ==  pMysql) {
         fprintf(stderr, "mysql_init error !");
         return 1;
     }
 
+
+    /*连接MYSQL*/
+    snResult = Mysql_connect(pMysql);
+    if(snResult != 0) {
+        printf("Mysql_connect error ! \n");
+    }
+
+    /*新增数据表*/
+    if('1' == argv[1][0]) {
+        snResult = Mysql_create(pMysql);
+        if(snResult != 0) {
+            printf("Mysql_create error ! \n");
+        }
+    }
+
+    /*插入数据*/
+    if('3' == argv[1][0]) {
+        snResult = Mysql_insert(pMysql);
+        if(snResult != 0) {
+            printf("Mysql_insert error ! \n");
+        }
+    }
+
+        return 0;
+}
+
+int Mysql_connect(MYSQL *pMysql) {
+    MYSQL  *psnResult = NULL;
+
+    char chServer[] = "localhost";
+    char chUser[] = "root";
+    char chPassword[] = "test123";
+    char chDatabase[] = "db_test";
+
     psnResult = mysql_real_connect(pMysql, chServer, chUser, chPassword, chDatabase, 0, NULL, 0);
-    if(NULL == psnResult) {
+    if(pMysql != psnResult) {
         fprintf(stderr, "mysql_real_connect error ! %s\n", mysql_error(pMysql));
         return 1;
     }
     else {
         printf("MySQL Connected!\n");
     }
-    if('1' == argv[1]) {
+}
+int Mysql_create(MYSQL *pMysql) {
+    int    snResult;
 
-        snResult = mysql_query(pMysql, "CREATE TABLE IF NOT EXISTS `history4` ( \
-                                     `id` int NOT NULL  AUTO_INCREMENT PRIMARY KEY, \
-                                     `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
-                                     `active` varchar(30) DEFAULT NULL, \
-                                     `who`    varchar(8) DEFAULT NULL \
+    snResult = mysql_query(pMysql, "CREATE TABLE IF NOT EXISTS `history4` ( \
+                                   `id` int NOT NULL  AUTO_INCREMENT PRIMARY KEY, \
+                                   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+                                   `active` varchar(30) DEFAULT NULL, \
+                                   `who`    varchar(8) DEFAULT NULL \
                                     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-        if(snResult != 0) {
-            fprintf(stderr, "mysql_query error ! %s\n", mysql_error(pMysql));
-            return 1;
-        }
-        else {
-            printf("NEW TABLE CREATE SUCCESS !\n");
-        }
+    if(snResult != 0) {
+        fprintf(stderr, "mysql_query error ! %s\n", mysql_error(pMysql));
+        return 1;
+    }
+    else {
+        printf("NEW TABLE CREATE SUCCESS !\n");
     }
 
-    if('3' == argv[1]) {
-        snResult = mysql_query(pMysql, "INSERT INTO history4 (active, who) VALUES \
+    return 0;
+}
+
+int Mysql_insert(MYSQL *pMysql) {
+    long int snResult = 0;
+
+    snResult = mysql_query(pMysql, "INSERT INTO history4 (active, who) VALUES \
                                                   ('create the table','Lyz')");
-        if(snResult != 0) {
-            fprintf(stderr, "msyql_query error ! %s\n", mysql_error(pMysql));
-            return 1;
-        }
-        else {
-            printf("INSERT IS SUCCESS !\n");
-        }
+    if(snResult != 0) {
+        fprintf(stderr, "msyql_query error ! %s\n", mysql_error(pMysql));
+        return 1;
+    }
+    else {
+        printf("INSERT IS SUCCESS !\n");
     }
 
-        return 0;
+    return 0;
 }
